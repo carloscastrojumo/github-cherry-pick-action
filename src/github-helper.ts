80d81ca21e9f9f831d5237388c79eb1810eeb16d
Carlos Castro <carlos.castro@jumo.world>
2021-02-24 18:15:32 +0000

    Add action

---
+++ src/github-helper.ts
@@ -0,0 +1,112 @@
+import * as github from '@actions/github'
+import * as core from '@actions/core'
+
+const ERROR_PR_REVIEW_FROM_AUTHOR =
+  'Review cannot be requested from pull request author'
+
+export interface Inputs {
+  token: string
+  commitMessage: string
+  committer: string
+  author: string
+  branch: string
+  title: string
+  body: string
+  labels: string[]
+  assignees: string[]
+  reviewers: string[]
+  teamReviewers: string[]
+}
+
+export async function createPullRequest(
+  inputs: Inputs,
+  prBranch: string
+): Promise<void> {
+  const octokit = github.getOctokit(inputs.token)
+  if (process.env.GITHUB_REPOSITORY !== undefined) {
+    const [owner, repo] = process.env.GITHUB_REPOSITORY.split('/')
+
+    // Get PR title
+    const title = inputs.title
+      ? inputs.title
+      : github.context.payload &&
+        github.context.payload.pull_request &&
+        github.context.payload.pull_request.title
+    core.info(`Using body '${title}'`)
+
+    // Get PR body
+    const body = inputs.body
+      ? inputs.body
+      : github.context.payload &&
+        github.context.payload.pull_request &&
+        github.context.payload.pull_request.body
+    core.info(`Using body '${body}'`)
+
+    // Create PR
+    const pull = await octokit.pulls.create({
+      owner,
+      repo,
+      head: prBranch,
+      base: inputs.branch,
+      title,
+      body
+    })
+
+    // Apply labels
+    if (inputs.labels.length > 0) {
+      const prLabels =
+        github.context.payload &&
+        github.context.payload.pull_request &&
+        github.context.payload.pull_request.labels
+      if (prLabels) {
+        inputs.labels.concat(prLabels)
+      }
+      core.info(`Applying labels '${inputs.labels}'`)
+      await octokit.issues.addLabels({
+        owner,
+        repo,
+        issue_number: pull.data.number,
+        labels: inputs.labels
+      })
+    }
+
+    // Apply assignees
+    if (inputs.assignees.length > 0) {
+      core.info(`Applying assignees '${inputs.assignees}'`)
+      await octokit.issues.addAssignees({
+        owner,
+        repo,
+        issue_number: pull.data.number,
+        assignees: inputs.assignees
+      })
+    }
+
+    // Request reviewers and team reviewers
+    try {
+      if (inputs.reviewers.length > 0) {
+        core.info(`Requesting reviewers '${inputs.reviewers}'`)
+        await octokit.pulls.requestReviewers({
+          owner,
+          repo,
+          pull_number: pull.data.number,
+          reviewers: inputs.reviewers
+        })
+      }
+      if (inputs.teamReviewers.length > 0) {
+        core.info(`Requesting team reviewers '${inputs.teamReviewers}'`)
+        await octokit.pulls.requestReviewers({
+          owner,
+          repo,
+          pull_number: pull.data.number,
+          team_reviewers: inputs.teamReviewers
+        })
+      }
+    } catch (e) {
+      if (e.message && e.message.includes(ERROR_PR_REVIEW_FROM_AUTHOR)) {
+        core.warning(ERROR_PR_REVIEW_FROM_AUTHOR)
+      } else {
+        throw e
+      }
+    }
+  }
+}
--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,123 @@
 import * as core from '@actions/core'
-import {wait} from './wait'
+import * as io from '@actions/io'
+import * as exec from '@actions/exec'
+import * as utils from './utils'
+import {Inputs, createPullRequest} from './github-helper'
 
-async function run(): Promise<void> {
+const CHERRYPICK_EMPTY =
+  'The previous cherry-pick is now empty, possibly due to conflict resolution.'
+
+export async function run(): Promise<void> {
   try {
-    const ms: string = core.getInput('milliseconds')
-    core.debug(`Waiting ${ms} milliseconds ...`) // debug is only output if you set the secret `ACTIONS_RUNNER_DEBUG` to true
+    const inputs: Inputs = {
+      token: core.getInput('token'),
+      commitMessage: core.getInput('commit-message'),
+      committer: core.getInput('committer'),
+      author: core.getInput('author'),
+      branch: core.getInput('branch'),
+      title: core.getInput('title'),
+      body: core.getInput('body'),
+      labels: utils.getInputAsArray('labels'),
+      assignees: utils.getInputAsArray('assignees'),
+      reviewers: utils.getInputAsArray('reviewers'),
+      teamReviewers: utils.getInputAsArray('teamReviewers')
+    }
 
-    core.debug(new Date().toTimeString())
-    await wait(parseInt(ms, 10))
-    core.debug(new Date().toTimeString())
+    core.info(`Cherry pick into branch ${inputs.branch}!`)
 
-    core.setOutput('time', new Date().toTimeString())
+    const githubSha = process.env.GITHUB_SHA
+    const prBranch = `cherry-pick-${githubSha}`
+
+    // Configure the committer and author
+    core.startGroup('Configuring the committer and author')
+    const parsedAuthor = utils.parseDisplayNameEmail(inputs.author)
+    const parsedCommitter = utils.parseDisplayNameEmail(inputs.committer)
+    core.info(
+      `Configured git committer as '${parsedCommitter.name} <${parsedCommitter.email}>'`
+    )
+    await gitExecution(['config', '--global', 'user.name', parsedAuthor.name])
+    await gitExecution([
+      'config',
+      '--global',
+      'user.email',
+      parsedCommitter.email
+    ])
+    core.endGroup()
+
+    // Update  branchs
+    core.startGroup('Fetch all branchs')
+    await gitExecution(['remote', 'update'])
+    await gitExecution(['fetch', '--all'])
+    core.endGroup()
+
+    // Create branch new branch
+    core.startGroup(`Create new branch from ${inputs.branch}`)
+    await gitExecution(['checkout', '-b', prBranch, `origin/${inputs.branch}`])
+    core.endGroup()
+
+    // Cherry pick
+    core.startGroup('Cherry picking')
+    const result = await gitExecution([
+      'cherry-pick',
+      '-m',
+      '1',
+      '--strategy=recursive',
+      '--strategy-option=theirs',
+      `${githubSha}`
+    ])
+    if (result.exitCode !== 0 && !result.stderr.includes(CHERRYPICK_EMPTY)) {
+      throw new Error(`Unexpected error: ${result.stderr}`)
+    }
+    core.endGroup()
+
+    // Push new branch
+    core.startGroup('Push new branch to remote')
+    await gitExecution(['push', '-u', 'origin', `${prBranch}`])
+    core.endGroup()
+
+    // Create pull request
+    core.startGroup('Opening pull request')
+    await createPullRequest(inputs, prBranch)
+    core.endGroup()
   } catch (error) {
     core.setFailed(error.message)
   }
+}
+
+async function gitExecution(params: string[]): Promise<GitOutput> {
+  const result = new GitOutput()
+  const stdout: string[] = []
+  const stderr: string[] = []
+
+  const options = {
+    listeners: {
+      stdout: (data: Buffer) => {
+        stdout.push(data.toString())
+      },
+      stderr: (data: Buffer) => {
+        stderr.push(data.toString())
+      }
+    }
+  }
+
+  const gitPath = await io.which('git', true)
+  result.exitCode = await exec.exec(gitPath, params, options)
+  result.stdout = stdout.join('')
+  result.stderr = stderr.join('')
+
+  if (result.exitCode === 0) {
+    core.info(result.stdout.trim())
+  } else {
+    core.info(result.stderr.trim())
+  }
+
+  return result
+}
+
+class GitOutput {
+  stdout = ''
+  stderr = ''
+  exitCode = 0
 }
 
 run()
